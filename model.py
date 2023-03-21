f6ddffa632a14e8cc0b4acdc59d1609080d36e90
githublqs <645625872@qq.com>
2023-03-21 11:30:00 +0800

    Update model.py

--- model.py
+++ model.py
@@ -163,6 +163,7 @@ class DexiNed(nn.Module):
 
     def __init__(self):
         super(DexiNed, self).__init__()
+        #通道数从3=>64,宽高变为原来的一半
         self.block_1 = DoubleConvBlock(3, 32, 64, stride=2,)
         self.block_2 = DoubleConvBlock(64, 128, use_act=False)
         self.dblock_3 = _DenseBlock(2, 128, 256) # [128,256,100,100]
